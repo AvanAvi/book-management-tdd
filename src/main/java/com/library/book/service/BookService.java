bc9bc909b73dcd025dcae7810db7c896c08110a2
AvanAvi <theastrofreek@gmail.com>
2025-07-03 23:14:07 +0200

    GREEN: Implement BookService layer to pass all tests
    - Add BookNotFoundException with descriptive messages
    - Create BookService interface with CRUD operations
    - Implement BookServiceImpl with repository dependency

--- src/main/java/com/library/book/service/BookService.java
+++ src/main/java/com/library/book/service/BookService.java
@@ -1,5 +1,17 @@
 package com.library.book.service;
 
-public interface BookService {
+import com.library.book.entity.Book;
+import java.util.List;
 
+public interface BookService {
+    
+    List<Book> findAll();
+    
+    Book findById(Long id);
+    
+    Book save(Book book);
+    
+    void deleteById(Long id);
+    
+    Book findByIsbn(String isbn);
 }
---
+++ src/main/java/com/library/book/service/BookServiceImpl.java
@@ -0,0 +1,49 @@
+package com.library.book.service;
+
+import org.springframework.stereotype.Service;
+
+import com.library.book.entity.Book;
+import com.library.book.repository.BookRepository;
+import com.library.book.exception.BookNotFoundException;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    
+    private final BookRepository bookRepository;
+    
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+    
+    @Override
+    public Book findById(Long id) {
+        return bookRepository.findById(id)
+            .orElseThrow(() -> new BookNotFoundException("Book not found with id: " + id));
+    }
+    
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+    
+    @Override
+    public void deleteById(Long id) {
+        if (!bookRepository.findById(id).isPresent()) {
+            throw new BookNotFoundException("Book not found with id: " + id);
+        }
+        bookRepository.deleteById(id);
+    }
+    
+    @Override
+    public Book findByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+            .orElseThrow(() -> new BookNotFoundException("Book not found with ISBN: " + isbn));
+    }
+}
